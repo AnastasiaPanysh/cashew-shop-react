e95e3d86159df9b71b8edf2c67912b4361ceaf45
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-01-30 22:58:57 +0300

    add header, preview

---
+++ src/components/Category/Category.js
@@ -0,0 +1,12 @@
+import style from './Category.module.css'
+
+function Category() {
+    return (
+        <>
+            <h1>Categories</h1>
+            <div></div>
+        </>
+    )
+}
+
+export default Category
---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,11 @@
+import style from './Preview.module.css'
+
+function Preview() {
+    return (
+        <>
+            <div className={style["preview-img"]}></div>
+        </>
+    )
+}
+
+export default Preview
