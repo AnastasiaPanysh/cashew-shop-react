32bf02bd6bf4981a51b4b383a58112eadb54f924
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-02-02 20:03:45 +0300

    add

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,12 +1,36 @@
-import style from './Category.module.css'
+import style from './Category.module.css';
 
 function Category() {
-    return (
-        <>
-            <h1>Categories</h1>
-            <div></div>
-        </>
+    const click = () => {
+        alert('Success event');
+    }
+    return(
+        <div className={style['container']}>
+            <h1 className={style['h1']}>Categories</h1>
+            <div className={style['wrapper']}>
+                <div className={style['item1']}>
+                    <div className={style['img']}></div>
+                    <div className={style['btn']} onClick={click}>Electronics</div>
+                </div>
+                <div className={style['item2']}>
+                    <div className={style['img']}></div>
+                    <div className={style['btn']} onClick={click}>Fashion & accessories</div>
+                </div>
+                <div className={style['item3']}>
+                    <div className={style['img']}></div>
+                    <div className={style['btn']} onClick={click}>Beauty & health</div>
+                </div>
+                <div className={style['item4']}>
+                    <div className={style['img']}></div>
+                    <div className={style['btn']} onClick={click}>Home & furniture</div>
+                </div>
+            </div>
+            <div className={style['viewAllBtn']} onClick={click}>
+                <p>View all</p>
+                <div className={style['arrow']}></div>
+            </div>
+        </div>
     )
 }
 
-export default Category
+export default Category;
